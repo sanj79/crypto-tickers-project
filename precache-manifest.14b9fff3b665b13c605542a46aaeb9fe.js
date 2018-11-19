86f652af40fa563152435bf28e5fb0009388af4b
Sanjay Tripathi <sanjay.tripathi@geminisolutions.in>
2018-11-19 13:27:20 +0530

    Updates

---
+++ precache-manifest.14b9fff3b665b13c605542a46aaeb9fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a30cf148bcd6db6bcb9e",
+    "url": "/crypto-tickers-project/static/js/runtime~main.a30cf148.js"
+  },
+  {
+    "revision": "7520b1e6fbb931f43b01",
+    "url": "/crypto-tickers-project/static/js/main.7520b1e6.chunk.js"
+  },
+  {
+    "revision": "ee98088a8c26ff76c30e",
+    "url": "/crypto-tickers-project/static/js/1.ee98088a.chunk.js"
+  },
+  {
+    "revision": "7520b1e6fbb931f43b01",
+    "url": "/crypto-tickers-project/static/css/main.0ccb5f7e.chunk.css"
+  },
+  {
+    "revision": "ee98088a8c26ff76c30e",
+    "url": "/crypto-tickers-project/static/css/1.8d621acf.chunk.css"
+  },
+  {
+    "revision": "4f2ab0ce51668f4782410c50d4f7ca6c",
+    "url": "/crypto-tickers-project/index.html"
+  }
+];
