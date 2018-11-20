d661a79b9ee046e2d185dab22bfea42db796d36d
Sanjay Tripathi <sanjay.tripathi@geminisolutions.in>
2018-11-20 19:38:14 +0530

    Added basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Tickers from './containers/Tickers/Tickers';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter basename="/crypto-tickers-project/">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="container-fluid">
           <Layout>
             <Tickers />
