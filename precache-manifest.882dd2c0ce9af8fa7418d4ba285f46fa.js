b0ac4e9678dc454f1f4aaa09df63e338c1b77005
Sanjay Tripathi <sanjay.tripathi@geminisolutions.in>
2018-11-22 17:33:31 +0530

    Updates

--- precache-manifest.882dd2c0ce9af8fa7418d4ba285f46fa.js
+++ precache-manifest.882dd2c0ce9af8fa7418d4ba285f46fa.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/crypto-tickers-project/static/js/runtime~main.a30cf148.js"
   },
   {
-    "revision": "343acca7b2dab2f8768a",
-    "url": "/crypto-tickers-project/static/js/main.343acca7.chunk.js"
+    "revision": "ed3a578fe1965923d307",
+    "url": "/crypto-tickers-project/static/js/main.ed3a578f.chunk.js"
   },
   {
     "revision": "a0ee491cbb312eb1d59c",
     "url": "/crypto-tickers-project/static/js/1.a0ee491c.chunk.js"
   },
   {
-    "revision": "343acca7b2dab2f8768a",
-    "url": "/crypto-tickers-project/static/css/main.c0ae047c.chunk.css"
+    "revision": "ed3a578fe1965923d307",
+    "url": "/crypto-tickers-project/static/css/main.9dd31934.chunk.css"
   },
   {
     "revision": "a0ee491cbb312eb1d59c",
     "url": "/crypto-tickers-project/static/css/1.8bda388e.chunk.css"
   },
   {
-    "revision": "70a0a1b3384013ff7f42561e65293242",
+    "revision": "cca5d2d510a0d840a2d70b6cf5f0deef",
     "url": "/crypto-tickers-project/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crypto-tickers-project/precache-manifest.c7ba779ebb7c8b9e9d09435e702ffac3.js"
+  "/crypto-tickers-project/precache-manifest.882dd2c0ce9af8fa7418d4ba285f46fa.js"
 );
 
 workbox.clientsClaim();
